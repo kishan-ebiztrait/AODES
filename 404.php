abe0fe86de1f780def77e1cea2be45497ba39cf4
Mayurebiztrait <114912976+Mayurebiztrait@users.noreply.github.com>
2022-10-03 17:35:29 +0530

    test changes

--- 404.php
+++ 404.php
@@ -10,13 +10,14 @@
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
  * @package brator
+ * test message
  */
 
 get_header();
 ?>
 <div class="brator-blog-post-area error-section">
 	<div class="container-xxxl container-xxl container">
-		<div class="content-box">
+		<div class="content-box"> 
 			<h1><?php esc_html_e( '404', 'brator' ); ?></h1>
 			<h2><?php esc_html_e( 'We’re unable to find a page you are looking for, Try later or click the button.', 'brator' ); ?></h2>
 			<a href="<?php echo esc_url( get_home_url() ); ?>" class="theme-btn-one"><?php esc_html_e( 'Back to Home', 'brator' ); ?></a>
