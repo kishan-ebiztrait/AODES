ec314dd141bbf77db7fcd90f9a0eb5199bd4e26d
Mayurebiztrait <114912976+Mayurebiztrait@users.noreply.github.com>
2022-10-14 18:20:36 +0530

    home page accesories as category show now and Accessories, Manuals, Vehicle (Catalogue) as show category now all changes

--- template/home-page.php
+++ template/home-page.php
@@ -186,7 +186,15 @@ get_header(); ?>
           ACCESSORIES
         </h2>
         <?php $count = 1;
-            $args = array( 'post_type' => 'catalogs', 'posts_per_page' => 2,'post_status' => 'publish' ,'orderby'      => 'ID', 'order'=>'DESC');
+            $args = array( 'post_type' => 'catalogs', 'posts_per_page' => 2,
+            'post_status' => 'publish' ,'orderby'      => 'ID', 'order'=>'DESC',
+            'tax_query' => array(
+              array (
+                  'taxonomy' => 'catalogs_categories',
+                  'field' => 'slug',
+                  'terms' => 'accessories',
+              )
+          ),);
             $the_query = new WP_Query( $args );  ?>
             <?php if ( $the_query->have_posts() ) : ?>
             <?php while ( $the_query->have_posts() ) : $the_query->the_post(); 
